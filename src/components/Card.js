47b9eab9a4abfc43d5db97d92dc796a0634eddeb
Kartik Mathur <KartikMathur@MacBook-Air.local>
2019-03-24 01:03:45 +0530

    Edit card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import MaterialCard from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 import CardContent from "@material-ui/core/CardContent";
 import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
+import Textarea from "react-textarea-autosize";
 import { Draggable } from "react-beautiful-dnd";
-import { deleteCard } from "../actions";
+import { deleteCard, editCard } from "../actions";
 import { connect } from "react-redux";
 
 const Card = React.memo(({ text, id, listID, index, dispatch }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [cardText, setText] = useState(text);
+
   const handleDeleteCard = e => {
     dispatch(deleteCard(id, listID));
+  };
+
+  const closeForm = e => {
+    setIsEditing(false);
+  };
+
+  const handleChange = e => {
+    setText(e.target.value);
+  };
+
+  const saveCard = e => {
+    e.preventDefault();
+    dispatch(editCard(id, listID, cardText));
+    setIsEditing(false);
+  };
+
+  const renderEditForm = () => {
+    return (
+      <div>
+        <MaterialCard className="CardContainer input">
+          <Textarea
+            className="Textarea"
+            placeholder="Enter card title"
+            autoFocus
+            onBlur={closeForm}
+            value={cardText}
+            onChange={handleChange}
+            onFocus={function(e) {
+              var val = e.target.value;
+              e.target.value = '';
+              e.target.value = val;
+            }}
+          />
+        </MaterialCard>
+        <div>
+          <Button
+            className="AddButton"
+            onMouseDown={saveCard}
+            variant="contained"
+          >
+            Save
+          </Button>
+        </div>
+      </div>
+    );
   };
 
   const renderCard = () => {
@@ -20,6 +70,7 @@ const Card = React.memo(({ text, id, listID, index, dispatch }) => {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
+            onDoubleClick={() => setIsEditing(true)}
             className="CardContainer"
           >
             <MaterialCard>
@@ -29,6 +80,13 @@ const Card = React.memo(({ text, id, listID, index, dispatch }) => {
                 fontSize="small"
               >
                 delete
+              </Icon>
+              <Icon
+                className="EditButton"
+                onMouseDown={() => setIsEditing(true)}
+                fontSize="small"
+              >
+                edit
               </Icon>
               <CardContent>
                 <Typography gutterBottom>{text}</Typography>
@@ -40,7 +98,7 @@ const Card = React.memo(({ text, id, listID, index, dispatch }) => {
     );
   };
 
-  return renderCard();
+  return isEditing ? renderEditForm() : renderCard();
 });
 
 export default connect()(Card);
